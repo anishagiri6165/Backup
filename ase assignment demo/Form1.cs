e3b9be2d93eac09f486922298696298008dacb49
Anishagiri <anishagiri154@gmail.com>
2024-12-09 21:57:14 +0545

    Added BOOSE

--- ase assignment demo/Form1.cs
+++ ase assignment demo/Form1.cs
@@ -1,10 +1,16 @@
+using System.Diagnostics;
+using BOOSE;
+
 namespace ase_assignment_demo
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            Debug.WriteLine(AboutBOOSE.about());
+
         }
     }
 }
